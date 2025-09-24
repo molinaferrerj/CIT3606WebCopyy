6381afd33d7bebfb15ba9dee17a15d0542177be6
molinaferrerj <molinaferrerj@student.elms.edu>
2025-09-24 18:09:45 +0200

    Lab 3
    Lab 3

--- Lab 3 Currency Converter UI/script.js
+++ Lab 3 Currency Converter UI/script.js
@@ -1,5 +1,13 @@
-function currency() {
-   const convert =    
-          document.getElementById("amount").value;
-   alert("Amount " + convert);
-}   
+function convert() {
+  var usd = document.getElementById("usd").value;
+
+  if (usd === "" || isNaN(usd)) {
+    alert("Please enter a valid number in USD field");
+    return;
+  }
+
+  var rate = 0.85; 
+  var EUR = (usd * rate).toFixed(2);
+
+  document.getElementById("EUR").value = EUR;
+}
